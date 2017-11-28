674d474c3ef5b0ddf81055223aa9172331be53bb
Matt Quade <quadem09@gmail.com>
2017-11-28 21:51:19 +0000

    twitter keys

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # liri-node-app
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+    consumer_key: 'EYohv8rWApUyiPry2hazM1eeP',
+    consumer_secret: '<WRqEwXptqIG9ts1L9Dk88oOmhQysR2H9o38sNCTpxSFClWDnYL>',
+    access_token_key: '<935555526364155904-jk7k9D46AXeQHRzfHqSvYwnQdkU9jGQ>',
+    access_token_secret: '< TAZvpxa0WhUiSel5RsAymfd6ummhU52bguC9EmzuBMrZG>',
+}
+
+module.exports = twitterKeys;
